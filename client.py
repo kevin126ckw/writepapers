2a36c55d51196c76debb71cfb61ad2c6c7b65f8d
kevinchang <35583291@qq.com>
2025-04-07 19:04:30 +0800

    feat: 添加客户端和服务端代码模板
    - 新建 client.py 和 server.py 两个文件
    - 添加基础的代码结构和注释信息- 定义主函数 main() 作为程序入口

---
+++ client.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/4/7
+# @File    : client.py
+# @Software: PyCharm
+# @Desc    :
+# @Author  : Kevin Chang
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
